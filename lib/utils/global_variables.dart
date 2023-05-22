fccfdb2982e76de6a957d729d16505a8cbf2ca93
anjali5s <tandukaranjali5@gmail.com>
2023-05-22 16:09:13 +0545

    message profile added

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,6 +1,7 @@
  import 'package:firebase_auth/firebase_auth.dart';
  import 'package:flutter/material.dart';
- import '../pages/page1.dart';
+ import '../pages/anjali_profile.dart';
+import '../pages/page1.dart';
 import '../screens/auth/login_screen.dart';
  import '../pages/page3.dart';
  import '../pages/page2.dart';
@@ -14,7 +15,7 @@ List<Widget> homeScreenItems = [
  ImageUploadScreen(),
   const AddPostScreen(),
   //const Text('notifications'),
-  //  ProfileScreen(
-  //    uid: FirebaseAuth.instance.currentUser!.uid,
-  //  ),
+   ProfileScreen(
+      uid: FirebaseAuth.instance.currentUser!.uid,
+    ),
 ];
