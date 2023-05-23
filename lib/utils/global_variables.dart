23e84d0bdbec39879f6e3928a811fadc916c118b
Anjali Tamang <lopchananjali@gmail.com>
2023-05-24 00:15:24 +0545

    explore screen added

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,21 +1,22 @@
- import 'package:firebase_auth/firebase_auth.dart';
- import 'package:flutter/material.dart';
- import '../pages/anjali_profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../pages/anjali_profile.dart';
 import '../pages/page1.dart';
 import '../screens/auth/login_screen.dart';
- import '../pages/page3.dart';
- import '../pages/page2.dart';
- import '../pages/page4.dart';
+import '../pages/page3.dart';
+import '../pages/page2.dart';
+import '../pages/page4.dart';
+import '../screens/explore_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const FeedScreen(),
- 
- ImageUploadScreen(),
+  const SearchScreen(),
+  ImageUploadScreen(),
   const AddPostScreen(),
   //const Text('notifications'),
-   ProfileScreen(
-      uid: FirebaseAuth.instance.currentUser!.uid,
-    ),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
